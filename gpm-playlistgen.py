4bc58da0f0ecb7479e96e8f7084e5d384c867c11
Hugo Haas <hugoh@hugoh.net>
2017-04-17 05:18:20 -0700

    Made PEP happy

--- gpm-playlistgen.py
+++ gpm-playlistgen.py
@@ -6,19 +6,23 @@ import yaml
 import sys
 from gpmplgen.gpmplgen import GPMPlGen
 
+
 def build_argparser():
     parser = argparse.ArgumentParser(description='Create playlists automatically.')
     parser.add_argument('config', type=file, help='Config file')
     parser.add_argument('--dry-run', action='store_true', help="Don't do any actual changes")
-    parser.add_argument('--delete-all-playlists', action='store_true', help="Delete all generated playlists and exit immediately")
+    parser.add_argument('--delete-all-playlists', action='store_true',
+                        help="Delete all generated playlists and exit immediately")
     parser.add_argument('--force', action='store_true', help="Regenerate all playlists")
     return parser
+
 
 def get_cache_path(cfg, cache_type):
     try:
         return cfg['dev'][cache_type]
-    except:
+    except KeyError:
         return None
+
 
 if __name__ == '__main__':
     argparser = build_argparser()
@@ -26,7 +30,7 @@ if __name__ == '__main__':
     cfg = yaml.load(args.config)
     debug_level = logging.INFO
     try:
-        if cfg['dev']['debug'] == True:
+        if cfg['dev']['debug']:
             debug_level = logging.DEBUG
     except:
         pass
