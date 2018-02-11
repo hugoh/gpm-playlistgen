ed4988d21c0525659f72682d70af63fb4c54f99f
Hugo Haas <hugoh@hugoh.net>
2018-02-11 09:19:22 -0800

    Moving to setup.py; step 1

---
+++ setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+from setuptools import find_packages, setup
+
+about = {}
+with open('gpmplgen/__version__.py') as f:
+    exec(f.read(), about)
+
+setup(name='GPM-Playlist-Generator',
+      version=about['__version__'],
+      description='Google Play Music - Playlist Generator',
+      author='Hugo Haas',
+      author_email='hugoh@hugoh.net',
+      url='https://gitlab.com/hugoh/gpm-playlistgen',
+      packages=find_packages(),
+      scripts=[
+          'scripts/gpm-playlistgen.py'
+      ],
+      install_requires=[
+          'gmusicapi',
+          'pyyaml'
+      ],
+      classifiers=[
+          'Environment :: Console',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 3',
+      ]
+      )
