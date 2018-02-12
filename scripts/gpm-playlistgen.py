c6fc14cfa2f7ff9c1a7215e624ddf9134fdb537a
Hugo Haas <hugoh@hugoh.net>
2018-02-11 16:20:45 -0800

    First try for upload to Pypi

--- scripts/gpm-playlistgen.py
+++ scripts/gpm-playlistgen.py
@@ -3,11 +3,13 @@
 import argparse
 import sys
 from gpmplgen import *
+import gpmplgen.__version__
 import logging
 
 
 def build_argparser():
-    parser = argparse.ArgumentParser(description='Create playlists automatically.')
+    parser = argparse.ArgumentParser(description='Create playlists automatically. Version: '
+                                                 + gpmplgen.__version__.__version__)
     parser.add_argument('config', type=argparse.FileType("r"), help='Config file')
     parser.add_argument('--dry-run', action='store_true', help="Don't do any actual changes")
     parser.add_argument('--delete-all-playlists', action='store_true',
--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 from setuptools import find_packages, setup
+import pypandoc
 
 about = {}
 with open('gpmplgen/__version__.py') as f:
@@ -9,6 +10,7 @@ with open('gpmplgen/__version__.py') as f:
 setup(name='GPM-Playlist-Generator',
       version=about['__version__'],
       description='Google Play Music - Playlist Generator',
+      long_description = pypandoc.convert_file('README.md', 'rst', format='gfm'),
       author='Hugo Haas',
       author_email='hugoh@hugoh.net',
       url='https://gitlab.com/hugoh/gpm-playlistgen',
@@ -20,9 +22,13 @@ setup(name='GPM-Playlist-Generator',
           'gmusicapi',
           'pyyaml'
       ],
+      keywords=['google music', 'google play music', 'playlist', 'playlist generator'],
       classifiers=[
           'Environment :: Console',
           'Programming Language :: Python',
+          'Programming Language :: Python :: 2',
           'Programming Language :: Python :: 3',
+          'Intended Audience :: End Users/Desktop',
+          'Topic :: Multimedia :: Sound/Audio',
       ]
       )
